32fcd78c80ab09261eda7174a4455f6465ea7308
CEROOOOOO <cero301205@gmail.com>
2025-11-14 03:40:52 +0530

    Refactor Hardhat config for new Solidity version
    Updated Hardhat configuration to use Solidity version 0.8.28 and removed network settings.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
-const { SEPOLIA_RPC_URL, PRIVATE_KEY } = process.env;
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    hardhat: {},
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-    sepolia: SEPOLIA_RPC_URL && PRIVATE_KEY ? {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    } : undefined,
-  }
+  solidity: "0.8.28",
 };
